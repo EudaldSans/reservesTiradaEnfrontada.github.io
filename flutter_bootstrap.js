671fee2b81d187da05404b3836dfebdd987b7c40
Eudald Sans <eudald.sans@gmail.com>
2025-05-13 06:55:50 +0200

    try profile without canvaskit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2868866179"
+    serviceWorkerVersion: "1395107032"
   }
 });
