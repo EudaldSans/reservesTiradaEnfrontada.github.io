715f7e787b389e5f12eeb94b7704deabe5c4c473
Eudald Sans <eudald.sans@gmail.com>
2025-05-13 19:37:15 +0200

    tried to guess better the size for the scaffolds

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3516341644"
+    serviceWorkerVersion: "1382420756"
   }
 });
