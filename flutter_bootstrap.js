f8241d28dd0ba377bacc0f1f2cc2b559d52fd775
Eudald Sans <eudald.sans@gmail.com>
2025-05-31 23:02:59 +0200

    Implemented user profile and deletion screen, fixed signup issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "168373979"
+    serviceWorkerVersion: "2851610708"
   }
 });
