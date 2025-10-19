9663e67ee980d3e92bcb1f39e4ccc61592b3eb59
Eudald Sans <eudald.sans@gmail.com>
2025-10-19 21:18:57 +0200

    Make add reservation scrollable

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "512569328"
+    serviceWorkerVersion: "4077044971"
   }
 });
